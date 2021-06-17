49a8654aa84775f3a470bd9f1b8ff84418689379
JessickaMoya <jessicka.moya@gmail.com>
2021-06-16 22:02:48 -0400

    Add config.properties file

--- src/test/java/ProjectTest.java
+++ src/test/java/ProjectTest.java
@@ -3,25 +3,18 @@ import api.ApiMethod;
 import api.ApiRequest;
 import api.ApiResponse;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class ProjectTest {
 
     ApiRequest apiRequest = new ApiRequest();
 
-    @BeforeTest
+    @BeforeMethod
     public void setGeneralConfig() {
-        apiRequest.addHeader("X-TrackerToken", "9f14b3ef7ece5f8404f8e1082f1a404b");
-        apiRequest.setBaseUri("https://www.pivotaltracker.com/services/v5");
-    }
-
-    @Test
-    public void itShouldGetAllProjects() {
-        apiRequest.setMethod(ApiMethod.GET);
-        apiRequest.setEndpoint("/projects");
-        ApiResponse apiResponse = new ApiResponse(ApiManager.execute(apiRequest));
-        Assert.assertEquals(apiResponse.getStatusCode(), 200);
+        ConfigFile configFile = new ConfigFile();
+        apiRequest = new ApiRequest();
+        apiRequest.addHeader("X-TrackerToken", configFile.getConfig().getProperty("PIVOTAL_TOKEN"));
+        apiRequest.setBaseUri(configFile.getConfig().getProperty("PIVOTAL_BASE_URI"));
     }
 
     @Test
@@ -29,7 +22,16 @@ public class ProjectTest {
         apiRequest.setEndpoint("/projects/{projectId}");
         apiRequest.setMethod(ApiMethod.GET);
         apiRequest.addPathParam("projectId", "2504505");
-        ApiResponse apiResponse = new ApiResponse(ApiManager.execute(apiRequest));
+        ApiResponse apiResponse = ApiManager.execute(apiRequest);
         Assert.assertEquals(apiResponse.getStatusCode(), 200);
     }
+
+    @Test
+    public void itShouldGetAllProjects() {
+        apiRequest.setMethod(ApiMethod.GET);
+        apiRequest.setEndpoint("/projects");
+        ApiResponse apiResponse = ApiManager.execute(apiRequest);
+        Assert.assertEquals(apiResponse.getStatusCode(), 200);
+    }
+
 }
